bccd78e4b96bf6d4e792d7418700d709cff06e08
yangyile <example@gogit.com>
2024-05-17 07:20:49 +0800

    添加DIR提供更简洁的选择

--- parent.go
+++ parent.go
@@ -4,15 +4,16 @@ import (
 	"path/filepath"
 )
 
+// 提供个类似于namespace的效果，这样能让函数名更简洁，使用起来意义也更明确，而且IDE代码提示的时候也能缩小选择范围
 type parentNamespace struct{}
 
-var PARENT = &parentNamespace{} //这里提供个全局变量以供外部使用，因为golang没有namespace的概念，但分包好像也没必要吧，就这样吧
+// 我觉得应该不会有使用者把这些全局变量设置为"nil"吧，啊哈哈哈，相信使用者不会给自己挖坑吧
+var (
+	PARENT = &parentNamespace{} //这里提供个全局变量以供外部使用，因为golang没有namespace的概念，但分包好像也没必要吧，就这样吧
+	DIR    = &parentNamespace{} //这是同义词嘛，因为在我的理解中可能 parent 表示父路径，但实际 DIR 更简单些
+)
 
 func (T *parentNamespace) Path() string {
-	return filepath.Dir(Skip(1))
-}
-
-func (T *parentNamespace) Root() string {
 	return filepath.Dir(Skip(1))
 }
 
--- parent_test.go
+++ parent_test.go
@@ -1,6 +1,7 @@
 package runpath
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -8,10 +9,6 @@ import (
 
 func Test_parentNamespace_Path(t *testing.T) {
 	t.Log(PARENT.Path())
-}
-
-func Test_parentNamespace_Root(t *testing.T) {
-	t.Log(PARENT.Root())
 }
 
 func Test_parentNamespace_Name(t *testing.T) {
@@ -26,4 +23,19 @@ func Test_parentNamespace_Skip(t *testing.T) {
 
 func Test_parentNamespace_Join(t *testing.T) {
 	t.Log(PARENT.Join("example.json"))
+}
+
+func Test_parentNamespace_Join1(t *testing.T) {
+	path := Path()
+	name := Name()
+	root := DIR.Path() // perhaps this variable should be named "dir", but "dir" is too ugly, more ugly than "ugly". so I prefer to use "root".
+	path2 := filepath.Join(root, name)
+	require.Equal(t, path, path2)
+}
+
+func Test_parentNamespace_Join2(t *testing.T) {
+	path := Path()
+	name := Name()
+	path2 := DIR.Join(name)
+	require.Equal(t, path, path2)
 }
