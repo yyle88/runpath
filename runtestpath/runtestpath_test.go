1ff5de742f794fe407401f440e45e8111c5617a5
yangyile <example@gogit.com>
2024-05-20 00:06:49 +0800

    修改文件名称

--- runtestpath/runtestpath_test.go
+++ runtestpath/runtestpath_test.go
@@ -10,35 +10,35 @@ import (
 func TestSrcPath(t *testing.T) {
 	path := SrcPath(t)
 	t.Log(path)
-	require.True(t, strings.HasSuffix(path, "runpath/runtestpath/utils_runtestpath.go"))
+	require.True(t, strings.HasSuffix(path, "runpath/runtestpath/runtestpath.go"))
 }
 
 func TestSrcName(t *testing.T) {
 	name := SrcName(t)
 	t.Log(name)
-	require.Equal(t, "utils_runtestpath.go", name)
+	require.Equal(t, "runtestpath.go", name)
 }
 
 func TestSrcSkip(t *testing.T) {
 	path := SrcSkip(t, 0)
 	t.Log(path)
-	require.True(t, strings.HasSuffix(path, "runpath/runtestpath/utils_runtestpath.go"))
+	require.True(t, strings.HasSuffix(path, "runpath/runtestpath/runtestpath.go"))
 }
 
 func TestSrcPathChangeExtension(t *testing.T) {
 	path := SrcPathChangeExtension(t, ".json")
 	t.Log(path)
-	require.True(t, strings.HasSuffix(path, "runpath/runtestpath/utils_runtestpath.json"))
+	require.True(t, strings.HasSuffix(path, "runpath/runtestpath/runtestpath.json"))
 }
 
 func TestSrcPathRemoveExtension(t *testing.T) {
 	path := SrcPathRemoveExtension(t)
 	t.Log(path)
-	require.True(t, strings.HasSuffix(path, "runpath/runtestpath/utils_runtestpath"))
+	require.True(t, strings.HasSuffix(path, "runpath/runtestpath/runtestpath"))
 }
 
 func TestSrcSkipRemoveExtension(t *testing.T) {
 	path := SrcSkipRemoveExtension(t, 0)
 	t.Log(path)
-	require.True(t, strings.HasSuffix(path, "runpath/runtestpath/utils_runtestpath"))
+	require.True(t, strings.HasSuffix(path, "runpath/runtestpath/runtestpath"))
 }
